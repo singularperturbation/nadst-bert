8d93913e1431ca1b36a3a3c08a7b291eb65cc4e2
Sloane Simmons <sloanes.k@gmail.com>
2022-01-02 16:24:35 -0600

    Package skeleton
    Add skeleton for python package.

---
+++ nadst_bert/__init__.py
@@ -0,0 +1,3 @@
+"""Top-level package for nadst-bert."""
+
+__version__ = "0.1.0"
---
+++ tests/test_nadst_bert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""Tests for `nadst_bert` package."""
+
+import pytest
+
+
+from nadst_bert import nadst_bert
+
+
+@pytest.fixture
+def response():
+    """Sample pytest fixture.
+
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    # import requests
+    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    """Sample pytest test function with the pytest fixture as an argument."""
+    # from bs4 import BeautifulSoup
+    # assert 'GitHub' in BeautifulSoup(response.content).title.string
